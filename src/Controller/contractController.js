1cc369de85a7c1102e5d2314a9f9fe4bf8f8fbf1
mohdsaif91 <saiffarooqui9505@gmail.com>
2023-05-15 23:10:22 +0530

    file reading tried but not sucessfull

--- src/Controller/contractController.js
+++ src/Controller/contractController.js
@@ -1,11 +1,21 @@
 exports.uploadContract = (req, res) => {
   try {
-    console.log("hinJANS");
-    console.log(req);
-    // let uploadFile = req.files;
-    // const name = uploadFile.name;
-    // console.log(name);
+    if (req.file) {
+      let uploadFile = req.file;
+      const name = uploadFile.name;
+      const strRegex = /function(.*?)```/gs;
+      console.log(strRegex.test(req.file.buffer.toString()));
+      //   for (let i = 0; i <= req.file.buffer.toString().length; i++) {
+      //     let substring = req.file.buffer.toString().substring(0, 10);
+      //     console.log(substring);
+      //   }
+      //   const solFileCode = req.file.buffer.toString().trim();
+      //   console.log(solFileCode.split("function"));
+    } else {
+      res.status(400).send("Image field is required");
+    }
   } catch (error) {
     console.log(error);
+    res.status(500).send(error);
   }
 };

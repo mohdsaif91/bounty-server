a556da46b97f4d515a8a180c82f2cf144c3ab343
Mohd Saif Farooqui <saif.farooqui@bristlecone.com>
2023-05-16 15:37:33 +0530

    function name getting

--- src/Controller/contractController.js
+++ src/Controller/contractController.js
@@ -3,14 +3,14 @@ exports.uploadContract = (req, res) => {
     if (req.file) {
       let uploadFile = req.file;
       const name = uploadFile.name;
-      const strRegex = /function(.*?)```/gs;
-      console.log(strRegex.test(req.file.buffer.toString()));
-      //   for (let i = 0; i <= req.file.buffer.toString().length; i++) {
-      //     let substring = req.file.buffer.toString().substring(0, 10);
-      //     console.log(substring);
-      //   }
-      //   const solFileCode = req.file.buffer.toString().trim();
-      //   console.log(solFileCode.split("function"));
+      const splitedCode = req.file.buffer.toString().split("\n");
+      const functionNameArray = splitedCode.filter((m) => {
+        // console.log(m);
+        if (m.includes("function")) {
+          return m;
+        }
+      });
+      res.status(200).send(functionNameArray);
     } else {
       res.status(400).send("Image field is required");
     }
